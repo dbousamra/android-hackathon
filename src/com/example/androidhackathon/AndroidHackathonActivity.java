9b503a613f5ff21621cfddf6b45b10b7108c5577
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-10-05 10:49:32 +1000

    Renamed packages

--- src/com/example/androidhackathon/AndroidHackathonActivity.java
+++ src/com/example/androidhackathon/AndroidHackathonActivity.java
@@ -2,17 +2,16 @@ package com.example.androidhackathon;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.example.AndroidHackathon.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
-public class AndroidHackathon extends Activity {
+public class AndroidHackathonActivity extends Activity {
     private GoogleMap map;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(com.example.androidhackathon.R.layout.main);
         this.map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
     }
 }
