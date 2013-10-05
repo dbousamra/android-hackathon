346fdd71f47f4e6f43b9e0d7475c4d542232a0c5
Mary-Anne Cosgrove <mcosgro@thoughtworks.com>
2013-10-05 12:44:57 +1000

    Location

--- src/com/example/androidhackathon/AndroidHackathonActivity.java
+++ src/com/example/androidhackathon/AndroidHackathonActivity.java
@@ -1,16 +1,30 @@
 package com.example.androidhackathon;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.androidhackathon.R;
 
-public class AndroidHackathonActivity extends Activity {
+public class AndroidHackathonActivity extends Activity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
     private GoogleMap map;
 
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationClient mLocationClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,5 +37,34 @@ public class AndroidHackathonActivity extends Activity {
             .title("Hello world"));
 
         Toast.makeText(this, PhotoStore.getCameraImages(this).size() + "", Toast.LENGTH_LONG).show();
+        mLocationClient = new LocationClient(this, this, this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mLocationClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+    public void onConnected(Bundle bundle) {
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.addMarker(new MarkerOptions()
+            .position(new LatLng(0, 0))
+            .title("Hello world"));
+    }
+
+    public void onDisconnected() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
